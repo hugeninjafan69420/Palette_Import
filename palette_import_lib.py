051b000ffafd50d7bbaf4db38d88afedac2f81fe
hujeninjaname69420 <slaycustoms@gmail.com>
2018-12-05 10:59:56 -0500

    Tyler and I good students,used package teletype to work on this together

--- palette_import_lib.py
+++ palette_import_lib.py
@@ -2,7 +2,9 @@
 functions to read the a CSV file
 and put it into a dictionary
 """
-
+import os
+import csv
+my_dict={}
 
 def read_palette_from_file(palette_filename):
     """
@@ -10,4 +12,14 @@ def read_palette_from_file(palette_filename):
     Each row is a key and an RGB triple.  Make that RGB triple
     into a TUPLE and then put into a dictionary with the key.
     """
-    pass
+    with open(palette_filename, 'r')  as f:
+        read_csv= csv.reader(f)
+        for row in read_csv:
+            key=int(row[0])
+            red=(row[1]).lstrip()
+            green=(row[2]).lstrip()
+            blue=(row[3]).lstrip()
+            my_dict[key]=red, green, blue
+
+
+    return my_dict
